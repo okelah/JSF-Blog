9a7d9b9e6be6051cb62f1d16484a6843bfd15ef6
Vladislav Sidlyarevich <vlsidlyarevich@gmail.com>
2016-08-21 00:46:16 +0300

    #2 - app configured to use h2 console

---
+++ src/main/java/com/github/vlsidlyarevich/spring/boot/jsf/blog/app/configuration/WebConfiguration.java
@@ -0,0 +1,19 @@
+package com.github.vlsidlyarevich.spring.boot.jsf.blog.app.configuration;
+
+import org.h2.server.web.WebServlet;
+import org.springframework.boot.context.embedded.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by vlad on 21.08.16.
+ */
+@Configuration
+public class WebConfiguration {
+    @Bean
+    ServletRegistrationBean h2servletRegistration() {
+        ServletRegistrationBean registrationBean = new ServletRegistrationBean(new WebServlet());
+        registrationBean.addUrlMappings("/console/*");
+        return registrationBean;
+    }
+}
