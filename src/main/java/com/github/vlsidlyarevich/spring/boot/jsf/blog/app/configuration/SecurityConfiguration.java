57a918ca51988bb369ba97da681e3428dbb82c55
Vladislav Sidlyarevich <vlsidlyarevich@gmail.com>
2016-08-21 00:41:18 +0300

    #2 - app configured to use h2 console

---
+++ src/main/java/com/github/vlsidlyarevich/spring/boot/jsf/blog/app/configuration/SecurityConfiguration.java
@@ -0,0 +1,22 @@
+package com.github.vlsidlyarevich.spring.boot.jsf.blog.app.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * Created by vlad on 21.08.16.
+ */
+@Configuration
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.authorizeRequests().antMatchers("/").permitAll().and()
+                .authorizeRequests().antMatchers("/console/**").permitAll();
+
+        httpSecurity.csrf().disable();
+        httpSecurity.headers().frameOptions().disable();
+    }
+
+}
