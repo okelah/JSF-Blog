658dcc7f0fc2cdd1f0170282fdae4023c9c91822
Vladislav Sidlyarevich <vlsidlyarevich@gmail.com>
2016-08-22 21:45:32 +0300

    #6 - dto added

---
+++ src/main/java/com/github/vlsidlyarevich/spring/boot/jsf/blog/app/dto/CommentDTO.java
@@ -0,0 +1,57 @@
+package com.github.vlsidlyarevich.spring.boot.jsf.blog.app.dto;
+
+import java.io.Serializable;
+
+/**
+ * Created by vlad on 22.08.16.
+ */
+public class CommentDTO implements Serializable {
+    private Long id;
+    private String author;
+    private String review;
+    private String date;
+    private Integer postId;
+
+    public CommentDTO() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getReview() {
+        return review;
+    }
+
+    public void setReview(String review) {
+        this.review = review;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public Integer getPostId() {
+        return postId;
+    }
+
+    public void setPostId(Integer postId) {
+        this.postId = postId;
+    }
+}
