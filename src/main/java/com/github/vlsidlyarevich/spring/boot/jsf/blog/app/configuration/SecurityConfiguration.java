7ebb3d28484c0baaf3054ad4e6379f862e3cfbb7
Vladislav Sidlyarevich <vlsidlyarevich@gmail.com>
2016-09-06 17:25:50 +0300

    #13 - spring security configuration

--- src/main/java/com/github/vlsidlyarevich/spring/boot/jsf/blog/app/configuration/SecurityConfiguration.java
+++ src/main/java/com/github/vlsidlyarevich/spring/boot/jsf/blog/app/configuration/SecurityConfiguration.java
@@ -1,8 +1,10 @@
 package com.github.vlsidlyarevich.spring.boot.jsf.blog.app.configuration;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 /**
  * Created by vlad on 21.08.16.
@@ -17,12 +19,25 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .antMatchers("/**/index.xhtml").permitAll()
                 .antMatchers("/**/about.xhtml").permitAll()
                 .antMatchers("/**/login.xhtml").permitAll()
-                .antMatchers("/**/posts.xhtml").authenticated()
                 .antMatchers("/**/new_post.xhtml").authenticated()
+                .antMatchers("/**/posts.xhtml").authenticated()
                 .antMatchers("/").permitAll();
+        httpSecurity
+                .formLogin().failureUrl("/views/login.xhtml?error")
+                .defaultSuccessUrl("/views/posts.xhtml")
+                .loginPage("/views/login.xhtml")
+                .permitAll()
+                .and()
+                .logout().logoutRequestMatcher(new AntPathRequestMatcher("/views/logout.xhtml")).logoutSuccessUrl("/views/login.xhtml")
+                .permitAll();
 
         httpSecurity.csrf().disable();
         httpSecurity.headers().frameOptions().disable();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication().withUser("user").password("password").roles("USER");
     }
 
 }
