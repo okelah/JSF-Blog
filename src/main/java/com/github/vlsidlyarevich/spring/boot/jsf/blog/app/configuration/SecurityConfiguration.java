800e7afcac0017a51f480bf3a213eeaedef0e03f
Vladislav Sidlyarevich <vlsidlyarevich@gmail.com>
2016-09-07 22:34:24 +0300

    master: security configuration update

--- src/main/java/com/github/vlsidlyarevich/spring/boot/jsf/blog/app/configuration/SecurityConfiguration.java
+++ src/main/java/com/github/vlsidlyarevich/spring/boot/jsf/blog/app/configuration/SecurityConfiguration.java
@@ -1,9 +1,12 @@
 package com.github.vlsidlyarevich.spring.boot.jsf.blog.app.configuration;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 /**
@@ -11,6 +14,9 @@ import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
  */
 @Configuration
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private UserDetailsService userDetailsService;
 
     @Override
     protected void configure(HttpSecurity httpSecurity) throws Exception {
@@ -37,7 +43,8 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication().withUser("user").password("password").roles("USER");
+        auth.userDetailsService(userDetailsService)
+                .passwordEncoder(new BCryptPasswordEncoder());
     }
 
 }
